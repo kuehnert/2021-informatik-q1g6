4fa5cc3af79aae270fc61380c5b5289011982b53
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-09-09 12:26:50 +0200

    Stunde vom 09.09.21

---
+++ src/HalloWelt.java
@@ -0,0 +1,11 @@
+public class HalloWelt {
+    public static void main(String[] args) {
+        int erg = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            erg = erg + i * i;
+        }
+
+        System.out.println("Das Ergebnis ist " + erg);
+    }
+}
---
+++ src/LineareSuche.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class LineareSuche {
+    Scanner scanner;
+    double[] zahlen;
+
+    public LineareSuche() {
+        scanner = new Scanner(System.in);
+        zahlen = new double[]{3.4, 5.0, 0.6, 19.6, 13, 6.6, 9.8};
+        for (int i = 0; i < zahlen.length; i++) {
+            zahlen[i] = zahlen[i] * 6.0 / 2.0 / 2.0 / 3.0;
+            System.out.println(zahlen[i]);
+        }
+
+        System.out.print("Gesuchte Zahl: ");
+        double eingabe = scanner.nextDouble();
+
+        int erg = lineareSuche(zahlen, eingabe);
+
+        System.out.println(eingabe + " ist an Stelle " + erg + " vorhanden.");
+    }
+
+    /**
+     * Die Methode durchsucht ein Array von Kommazahlen nach einer gesuchten Zahl
+     * Wenn die Zahl enthalten ist, gibt sie den Index im Array zurück,
+     * ansonsten -1, wenn sie nicht enthalten ist.
+     * @param a das Array, das durchsucht werden soll
+     * @param gesucht die gesuchte Zahl
+     * @return Index oder -1
+     */
+    public int lineareSuche(double[] a, double gesucht) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == gesucht) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public int lineareSucheTobias(double[] a, double gesucht) {
+        for (int i = 0; i < a.length; i++) {
+            if ((int) (a[i] * 100_000) == (int) (gesucht * 100_000)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        new LineareSuche();
+    }
+}

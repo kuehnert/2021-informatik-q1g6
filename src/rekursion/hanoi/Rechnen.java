9387aba1b3d9813fb1c9e7e149674525b600d2e5
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-02-07 11:47:34 +0100

    Stunde vom 07.02.2022

--- src/rekursion/hanoi/Rechnen.java
+++ src/rekursion/hanoi/Rechnen.java
@@ -1,30 +1,34 @@
 package rekursion.hanoi;
 
 public class Rechnen {
-    public static long hanoiBewgegungenFormel(int anzahl) {
+    public static long hanoiBewegungenFormel(int anzahl) {
         // 2^anzahl - 1
-        return -1;
+        return (long) Math.pow(2, anzahl) - 1;
     }
 
-    public static long hanoiBewgegungenIterativ(int anzahl) {
-        int counter = 0;
-        for (int i = 0; i < 10; i++) {
-            counter -= 10;
+    public static long hanoiBewegungenIterativ(int anzahl) {
+        long counter = 0;
+
+        for (int i = 0; i < anzahl; i++) {
+            counter = counter + 1 + counter;
         }
 
         return counter;
     }
 
-    public static long hanoiBewgegungenRekursiv(int anzahl) {
-        int wuerg = -10;
-        return wuerg;
+    public static long hanoiBewegungenRekursiv(int anzahl) {
+        if (anzahl == 1) {
+            return 1;
+        } else {
+            return 1 + hanoiBewegungenRekursiv(anzahl - 1) * 2;
+        }
     }
 
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
-            System.out.println(hanoiBewgegungenFormel(i));
-            System.out.println(hanoiBewgegungenIterativ(i));
-            System.out.println(hanoiBewgegungenRekursiv(i));
+            System.out.println("formel: " + hanoiBewegungenFormel(i));
+            System.out.println("iter.: " + hanoiBewegungenIterativ(i));
+            System.out.println("rek.: " +  hanoiBewegungenRekursiv(i));
         }
     }
 }

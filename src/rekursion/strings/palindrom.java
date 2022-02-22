d4b5d16bc21e475dab2f9b245a5b14030bea8b83
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-02-22 08:23:54 +0100

    Stunde vom 22.02.2022

--- src/rekursion/strings/palindrom.java
+++ src/rekursion/strings/palindrom.java
@@ -13,15 +13,32 @@ public class palindrom {
         }
     }
 
+    private static void _permutationen(String finiss, String remainder) {
+        if (remainder.length() == 0) {
+            System.out.println(finiss);
+        } else {
+            for (int i = 0; i < remainder.length(); i++) {
+                char current = remainder.charAt(i);
+                String newRemainder = remainder.substring(0, i) + remainder.substring(i+1, remainder.length());
+                _permutationen(finiss + current, newRemainder);
+            }
+        }
+    }
+
+    public static void permutationen(String wort) {
+        _permutationen("", wort);
+    }
+    
     // lagerregal = lagerregal
     // hannah
     // madamimadam
     // einnegermitgazellezagtimregennie
     public static void main(String[] args) {
-        System.out.println(isPalindrome("hallo")); //=> false
-        System.out.println(isPalindrome("anna")); //=> true
-        System.out.println(isPalindrome("ana")); //=> true
-        System.out.println(isPalindrome("abc")); //=> false
-        System.out.println(isPalindrome("lagerregal")); //=> true
+        // System.out.println(isPalindrome("hallo")); //=> false
+        // System.out.println(isPalindrome("anna")); //=> true
+        // System.out.println(isPalindrome("ana")); //=> true
+        // System.out.println(isPalindrome("abc")); //=> false
+        // System.out.println(isPalindrome("lagerregal")); //=> true
+        permutationen("help");
     }
 }
